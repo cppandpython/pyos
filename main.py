77926cec9cfbc7372eff16a082ac4958529e771c
ΨλΩδζΣλΩ∆ <168195999+cppandpython@users.noreply.github.com>
2025-11-14 06:49:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import sys;sys.dont_write_bytecode=True;from pprint import pprint;from time import time;time_start=time();import pyos
-# В класах использовать __slots__ = () для атрибутов класса и все классы должны подержывать with
-# !!!Писать без повторени и эффективный код!!!
-# invalid_type('', 1.5, {'int'})  типы только передаются set
-# по пробывать сделать запуск от прав системы
-
-
-
-
-
-
-
-
-# with open('../pyos.zip', 'rb') as f:
-#     pyos.tool.Clipboard.copy(str(f.read()))
-print('\n\ntime:', str(round(time() - time_start, 2)) + 's', flush=True)
+import pyos
